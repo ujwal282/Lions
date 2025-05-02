a7769b808a89eda52e7a88f546de8ad89d5c5297
ujwal282 <ujwalpaudel65@gmail.com>
2025-05-02 17:11:09 +0545

    fix video on home page

--- src/Components/Home/Herop.jsx
+++ src/Components/Home/Herop.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
+
 const Herop = () => {
   return (
     <section className="w-full h-max xl:h-lvh mt-32">
-    <div className="w-full flex items-center justify-center">
-      <video
-        className="h-[600px] w-full object-cover"
-        playsInline
-        preload="auto"
-        autoPlay
-        loop
-        muted
-        src="/images/lions video.mp4"
-        title="Lions in the wild"
-      />
-    </div>
-  </section>
-  
+      <div className="w-full flex items-center justify-center">
+        <div className="w-full relative aspect-[3/2] sm:aspect-auto">
+          <video
+            className="w-full h-full object-cover"
+            playsInline
+            preload="auto"
+            autoPlay
+            loop
+            muted
+            src="/images/lions video.mp4"
+            title="Lions in the wild"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
